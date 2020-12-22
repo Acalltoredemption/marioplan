fb158ab593c64e33c604f8fc7c89997ea8afa352
Acalltoredemption <acalltoredemption@gmail.com>
2020-12-22 13:56:57 -0800

    signup functional

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import {signIn} from '../../store/actions/authActions';
-
+import { Redirect } from 'react-router-dom';
 
 
 class Signin extends Component {
@@ -19,7 +19,8 @@ state = {
         this.props.signIn(this.state);
     }
     render(){
-        const { authError } = this.props
+        const { authError, auth } = this.props;
+        if (auth.uid) return <Redirect to='/' />
         return(
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
@@ -45,7 +46,8 @@ state = {
 }
 const mapStateToProps = (state) => {
     return {
-        authError: state.auth.authError
+        authError: state.auth.authError,
+        auth: state.firebase.auth
     }
 }
 
