f218d3ef9eb87ec9bf8f6946b220f3365ff5ae1d
Acalltoredemption <acalltoredemption@gmail.com>
2020-12-22 11:46:20 -0800

    setting up login authentication

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {signIn} from '../../store/actions/authActions';
+
+
 
 class Signin extends Component {
 state = {
@@ -12,9 +16,10 @@ state = {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        this.props.signIn(this.state);
     }
     render(){
+        const { authError } = this.props
         return(
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
@@ -29,11 +34,27 @@ state = {
             </div>
             <div className="input-field">
                 <button className="btn pink lighten-1 z-depth-0">Login</button>
+                <div className="red-text center">
+                {authError ? <p>{authError}</p> : null}
+                </div>
             </div>
                 </form>
             </div>
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        authError: state.auth.authError
+    }
+}
 
-export default Signin;
+const mapDispatchToProps = (dispatch) => {
+return {
+    signIn: (creds) => dispatch(signIn(creds))
+}
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Signin);
