68fcda4f0715c025d7c64c3d71142b0c4e8e5f15
Acalltoredemption <acalltoredemption@gmail.com>
2020-12-21 19:23:14 -0800

    implemented ProjectDetails component and routed into App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
 
 
 function App() {
@@ -7,6 +9,10 @@ function App() {
     <BrowserRouter>
     <div className="App">
      <Navbar />
+     <Switch>
+       <Route exact path='/' component={Dashboard} />
+       <Route path='/project/:id' component={ProjectDetails} />
+     </Switch>
     </div>
     </BrowserRouter>
   ); 
